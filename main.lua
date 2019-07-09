eaacaea24a63fd9e5c52a359e7da269d648dbded
Humaid AlQassimi <me@humaidq.ae>
2019-07-09 17:22:00 +0400

    Smaller enemies

--- main.lua
+++ main.lua
@@ -45,7 +45,7 @@ function love.update(dt)
         canShoot = true
     end
 
-    if love.keyboard.isDown('space', 'rctrl', 'lctrl', 'ctrl') and canShoot then
+    if love.keyboard.isDown('space', 'rctrl', 'lctrl') and canShoot then
         newBullet = { x = player.x + (player.img:getWidth()/2), y = player.y, img = bulletImg }
         table.insert(bullets, newBullet)
         canShoot = false
