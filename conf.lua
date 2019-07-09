4b67f4cfd74e3030367acf82871ac9a60184e26f
abdelrahman <abood.wael123@gmail.com>
2019-07-09 16:34:12 +0400

    Add game base code

---
+++ conf.lua
@@ -0,0 +1,10 @@
+-- Configuration
+function love.conf(t)
+	t.title = "Pew Pew Shooter: Return Of the Pews"
+	t.version = "0.9.1"         
+	t.window.width = 480       
+    t.window.height = 800
+
+	-- For Windows debugging
+	t.console = true
+end
